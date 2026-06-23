7203783eb29bd59817d1053f5f2f693fb8ca3c3a
Benja <benjaminarrollo14@gmail.com>
2026-06-23 19:50:06 -0300

    Update Producto.java

--- prog2/entidad/Producto.java
+++ prog2/entidad/Producto.java
@@ -7,7 +7,7 @@ public class Producto extends Base {
     private int stock;
     private String imagen;
     private Boolean disponible;
-    private Categoria categoria; // Relación
+    private Categoria categoria; 
 
     public Producto() {
         super();
